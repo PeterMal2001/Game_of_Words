6157c91d86b85de2f19a1dfec4c9276ba1fa666a
PeterMal <PeterMal@mail.ru>
2019-04-11 19:24:53 +0300

    добавлены коментарии

--- Game of Words/Game_of_Words.py
+++ Game of Words/Game_of_Words.py
@@ -1,4 +1,5 @@
 used_words=[]
+print("Введите число игроков...")
 n=int(input())
 players=[i+1 for i in range(n)]
 win=False
@@ -6,18 +7,24 @@ new=False
 
 while win==False:
     for player in players:
+        print("Ход игрока "+str(player)+".")
         new=False
         if len(players)==1:
             win=True
             break
         while new==False:
+            print("Введите слово...")
             word=input()
             if word=="fin":
+                print("Игрок "+str(player)+" сдаётся и выбывает из игры.")
                 players.remove(player)
                 new=True
             elif not (word in used_words):
+                print("Слово принято.")
                 used_words.append(word)
                 new=True
                 winner=player
+            else:
+                print("Данное слово уже было введено.")
             
-print(winner)
+print("Игра окончена, побеждает игрок "+str(winner)+"!")
