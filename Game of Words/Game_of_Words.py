49862d8b111feaf0c30e771ca1d2d55d1476ada8
PeterMal <PeterMal@mail.ru>
2019-04-11 19:10:58 +0300

    создал код без комментариев

--- Game of Words/Game_of_Words.py
+++ Game of Words/Game_of_Words.py
@@ -1,1 +1,23 @@
+used_words=[]
+n=int(input())
+players=[i+1 for i in range(n)]
+win=False
+new=False
 
+while win==False:
+    for player in players:
+        new=False
+        if len(players)==1:
+            win=True
+            break
+        while new==False:
+            word=input()
+            if word=="fin":
+                players.remove(player)
+                new=True
+            elif not (word in used_words):
+                used_words.append(word)
+                new=True
+                winner=player
+            
+print(winner)
