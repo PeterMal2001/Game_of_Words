eb0ed55fa78edce9c0251889e7622ba5cfcef8a6
PeterMal <PeterMal@mail.ru>
2019-04-12 18:49:45 +0300

    закончена 1 фаза и очищен экран во 2 фазе

--- Game of Words/Game_of_Words.py
+++ Game of Words/Game_of_Words.py
@@ -1,23 +1,41 @@
-used_words=[]
-n=int(input())
-players=[i+1 for i in range(n)]
-win=False
-new=False
+from sip import delete
+import sys
+from PyQt5.QtWidgets import QApplication,QMainWindow,QWidget,QPushButton,QLineEdit,QLabel,QGridLayout
 
-while win==False:
-    for player in players:
-        new=False
-        if len(players)==1:
-            win=True
-            break
-        while new==False:
-            word=input()
-            if word=="fin":
-                players.remove(player)
-                new=True
-            elif not (word in used_words):
-                used_words.append(word)
-                new=True
-                winner=player
-            
-print(winner)
+class kekapp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.widget=QWidget()
+        self.setCentralWidget(self.widget)
+
+        self.phase1()
+    def phase1(self):
+        self.layout=QGridLayout()
+        self.widget.setLayout(self.layout)
+        self.layout.setSpacing(10)
+
+        self.lbl1=QLabel("Введите количество игроков:")
+        self.layout.addWidget(self.lbl1,0,0,1,5)
+
+        self.pl_count=QLineEdit()
+        self.layout.addWidget(self.pl_count,1,1,1,3)
+
+        self.play=QPushButton("Играть")
+        self.layout.addWidget(self.play,3,3,1,2)
+        self.play.clicked.connect(self.pl_count_inserted)
+        self.show()
+
+    def phase2(self):
+        n=int(self.pl_count.text())
+        print("phase2")
+        self.layout.removeWidget(self.lbl1)
+        self.layout.removeWidget(self.pl_count)
+        self.layout.removeWidget(self.play)
+        delete(self.lbl1)
+        delete(self.pl_count)
+        delete(self.play)
+
+if __name__=="__main__":
+    app=QApplication(sys.argv)
+    kek=kekapp()
+    sys.exit(app.exec_())
